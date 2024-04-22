5d7ae9de07809e39446c6ac868553a00970f5a53
Dibya34 <156080237+Dibya34@users.noreply.github.com>
2024-04-22 13:41:02 +0530

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,21 +5,35 @@ export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
   return (
-    <header className="bg-red-200 flex justify-between">
-      <section className="">
-        <img className="w-20 h-30" src={AnimeLogo} alt="main_logo" />
-      </section>
-      <section className="flex justify-center items-center">
-        <ul className="list-none flex">
-          <li className="text-blue-500 mr-2">Home</li>
-          <li className="text-blue-500 mr-2">Latest Anime</li>
-          <li className="text-blue-500 mr-2">Current Feeds</li>
-        </ul>
-      </section>
-      <section className="flex justify-center items-center">
-        <Button>Hello</Button>
-        <Button>Hello</Button>
-      </section>
-    </header>
+    <div className="sticky inset-x-0 top-0 z-30 w-full transition-all border-b border-gray-200 bg-white/75 backdrop-blur-lg">
+      <div className="max-w-7xl mx-auto lg:px-8 px-6">
+        <div className="relative flex h-14 items-center justify-between">
+          <a
+            href="/"
+            className="relative sm:absolute inset-y-0 left-0 flex items-center font-semibold"
+          >
+            <img src={AnimeLogo} className="h-20 w-20 mr-0.5 " />
+            Anime News
+          </a>
+
+          <div className="hidden sm:block invisible">ProfanityAPI</div>
+
+          <div className="hidden sm:flex items-center gap-6">
+            <a className="hover:underline" href="#video-demo">
+              Latest Anime
+            </a>
+            <a className="hover:underline" href="#api">
+              Feeds
+            </a>
+          </div>
+          <div className="">
+            <Button variant="ghost">Log in</Button>
+            <Button className="bg-orange-500 hover:bg-orange-700">
+              Sign up
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
